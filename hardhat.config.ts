872dbf39e67253e174a1796f40fe8dd20e839a28
Yash Goyal <yashgo0018@gmail.com>
2024-01-21 16:25:15 +0530

    updated the hardhat config to support mumbai and sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,39 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const deployerPrivateKey =
+  process.env.DEPLOYER_PRIVATE_KEY ||
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.23",
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC || "",
+      accounts: [deployerPrivateKey],
+    },
+    "polygon-mumbai": {
+      url: process.env.POLYGON_MUMBAI_RPC || "",
+      accounts: [deployerPrivateKey],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+    },
+  },
 };
 
 export default config;
