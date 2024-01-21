9f042ac472dac5056a3ad9b48868e5c1dae8bb8a
Yash Goyal <yashgo0018@gmail.com>
2024-01-21 20:31:40 +0530

    added config to make the gho bridge contract a facilitator

--- scripts/02_set_config.ts
+++ scripts/02_set_config.ts
@@ -2,6 +2,7 @@ import hre from "hardhat";
 import { chains } from "./common";
 import fs from "fs";
 import type { Address } from "viem";
+import { parseEther } from "viem";
 
 async function setConfig() {
   const network = hre.network.name;
@@ -9,8 +10,10 @@ async function setConfig() {
   const chain = chains[network];
 
   if (!chain) throw `Chain ${network} is not supported`;
+  const account = (await hre.viem.getWalletClients())[0].account.address;
 
   let ghoBridgeAddress: Address;
+  let ghoTokenAddress: Address;
 
   try {
     const deploymentFile = fs.readFileSync(
@@ -19,11 +22,23 @@ async function setConfig() {
     );
     const deployment = JSON.parse(deploymentFile);
     ghoBridgeAddress = deployment.address;
+    ghoTokenAddress = deployment.token;
   } catch (e) {
     throw `The gho bridge contract is not yet deployed on ${network}`;
   }
 
-  // the config will go here
+  const token = await hre.viem.getContractAt("GhoToken", ghoTokenAddress);
+
+  await token.write.grantRole([
+    await token.read.FACILITATOR_MANAGER_ROLE(),
+    account,
+  ]);
+
+  await token.write.addFacilitator([
+    ghoBridgeAddress,
+    "bridge",
+    parseEther("10000000000000000"),
+  ]);
 }
 
 setConfig().catch((error) => {
