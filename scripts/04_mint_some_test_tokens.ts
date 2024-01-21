6f5702070a380e46f910092a2d36f1da405f5a4c
Yash Goyal <yashgo0018@gmail.com>
2024-01-21 20:31:55 +0530

    added a script to mint some test gho tokens

---
+++ scripts/04_mint_some_test_tokens.ts
@@ -0,0 +1,48 @@
+import hre from "hardhat";
+import { chains } from "./common";
+import fs from "fs";
+import type { Address } from "viem";
+import { parseEther } from "viem";
+
+async function mintSomeTestTokens() {
+  const network = hre.network.name;
+
+  const chain = chains[network];
+
+  if (!chain) throw `Chain ${network} is not supported`;
+
+  const account = (await hre.viem.getWalletClients())[0].account.address;
+
+  let ghoTokenAddress: Address;
+
+  try {
+    const deploymentFile = fs.readFileSync(
+      `./deployments/${network}.json`,
+      "utf-8"
+    );
+    const deployment = JSON.parse(deploymentFile);
+    ghoTokenAddress = deployment.token;
+  } catch (e) {
+    throw `The gho bridge contract is not yet deployed on ${network}`;
+  }
+
+  const token = await hre.viem.getContractAt("GhoToken", ghoTokenAddress);
+
+  await token.write.grantRole([
+    await token.read.FACILITATOR_MANAGER_ROLE(),
+    account,
+  ]);
+
+  await token.write.addFacilitator([
+    account,
+    "tester",
+    parseEther("1000000000"),
+  ]);
+
+  await token.write.mint([account, parseEther("1000000000")]);
+}
+
+mintSomeTestTokens().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
