65024133d157301c112118d421c74db9ec6a0cee
Yash Goyal <yashgo0018@gmail.com>
2024-01-21 17:21:33 +0530

    added a deployment script

---
+++ scripts/00_deploy_contracts.ts
@@ -0,0 +1,76 @@
+import hre from "hardhat";
+import fs from "fs";
+import path from "path";
+import { chains } from "./common";
+
+const ccipConfigs = {
+  "": {},
+};
+
+async function writeJSONToFile(
+  data: any,
+  fileName: string,
+  dirPath = "./deployments"
+) {
+  if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
+
+  const filePath = path.join(dirPath, fileName);
+
+  if (fs.existsSync(filePath)) return false;
+
+  fs.writeFileSync(filePath, JSON.stringify(data));
+  return true;
+}
+
+async function deployContracts() {
+  const network = hre.network.name;
+  console.log(network);
+
+  const chain = chains[network];
+
+  const admin = (await hre.viem.getWalletClients())[0].account.address;
+
+  const ghoToken = await hre.viem.deployContract("GhoToken", [admin], {
+    confirmations: 2,
+  });
+
+  console.log(`Gho Token is deployed to ${ghoToken.address}`);
+
+  try {
+    await hre.run("verify:verify", {
+      address: ghoToken.address,
+      constructorArguments: [admin],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  const ghoBridge = await hre.viem.deployContract(
+    "GhoBridge",
+    [chain.relayer, chain.linkToken, ghoToken.address],
+    { confirmations: 2 }
+  );
+
+  console.log(`Gho Bridge is deployed to ${ghoBridge.address}`);
+
+  try {
+    await hre.run("verify:verify", {
+      address: ghoBridge.address,
+      constructorArguments: [chain.relayer, chain.linkToken, ghoToken.address],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  // create the contract deployment file
+  const fileName = `${network}.json`;
+  const data = { token: ghoToken.address, address: ghoBridge.address };
+  writeJSONToFile(data, fileName);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+deployContracts().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/common.ts
@@ -0,0 +1,25 @@
+import type { Address } from "viem";
+
+interface ChainInfo {
+  relayer: Address;
+  linkToken: Address;
+  selector: string;
+}
+
+export const supportedNetworks: Record<number, string> = {
+  11155111: "sepolia",
+  80001: "polygon-mumbai",
+};
+
+export const chains: Record<string, ChainInfo> = {
+  sepolia: {
+    relayer: "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59",
+    linkToken: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+    selector: "16015286601757825753",
+  },
+  "polygon-mumbai": {
+    relayer: "0x1035CabC275068e0F4b745A29CEDf38E13aF41b1",
+    linkToken: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+    selector: "12532609583862916517",
+  },
+};
